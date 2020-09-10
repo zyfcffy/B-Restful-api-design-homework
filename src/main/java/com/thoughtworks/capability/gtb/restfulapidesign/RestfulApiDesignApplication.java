234653a8672de873b76651223ed62b8d26df99a7
Yuqian Zhu <yuqian.zhu@thoughtworks.com>
2020-09-10 22:06:20 +0800

    initialize project

---
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/RestfulApiDesignApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.capability.gtb.restfulapidesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiDesignApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RestfulApiDesignApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/thoughtworks/capability/gtb/restfulapidesign/RestfulApiDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.capability.gtb.restfulapidesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiDesignApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
